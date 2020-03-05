fa8afb62adced1d50330591fe25fa8324d237074
MARYRIVERO <mariyndlvalle892286@gmail.com>
2020-03-05 17:15:55 -0500

    agrega componente Logo

---
+++ burguer-queen/src/component-logo.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import logo from './logo.svg';
+import './Logo.css';
+
+function Logo() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+      </header>
+    </div>
+  );
+}
+
+export default Logo;
+
+
+
+
+
+
+
+
+
+
