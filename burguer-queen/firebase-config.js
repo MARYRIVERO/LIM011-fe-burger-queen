2f7996edaf6e6dde67e59fb0dc8e16a82d8e1b1a
MARYRIVERO <mariyndlvalle892286@gmail.com>
2020-03-06 17:50:53 -0500

    Inicia configuración de Firebase

---
+++ burguer-queen/firebase-config.js
@@ -0,0 +1,15 @@
+import firebase from 'firebase/app'
+import 'firebase/firestore'
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCvX8FT01Vxj_C015ToQQgoyhWwSkSEtd8",
+    authDomain: "burger-queen-ap.firebaseapp.com",
+    databaseURL: "https://burger-queen-ap.firebaseio.com",
+    projectId: "burger-queen-ap",
+    storageBucket: "burger-queen-ap.appspot.com",
+    messagingSenderId: "548403669843",
+    appId: "1:548403669843:web:fe659489c5f749a0f04fa6"
+  };
+
+  firebase.initializeApp(firebaseConfig);
+  export default firebase;
