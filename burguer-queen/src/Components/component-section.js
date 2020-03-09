8fe3356c15c379513402083dea51c333e7bbfdb6
MARYRIVERO <marilyndlvalle892286@gmail.com>
2020-03-09 18:28:48 -0500

    Actualiza codigo

--- burguer-queen/src/Components/component-section.js
+++ burguer-queen/src/Components/component-section.js
@@ -1,17 +1,13 @@
 import React from 'react';
 import './component-section.css';
-import FirestoreCollection from './component-list.js';
+// import FirestoreCollection from './component-list.js';
 
 function ListDes () {
   console.log('Desayuno');
 }
 
-function Firebase() {
-  return (
-    <div>
-    <FirestoreCollection />
-    </div>
-  )
+function ListAlm() {
+  console.log('Almuerzo y Cena');
 }
 
 
@@ -21,8 +17,8 @@ function Section() {
       <main>
           <h1>Lista de Productos</h1>
           <div className="Section-main" >
-          <button className="Button" onClick={Firebase}>Desayuno</button>
-          <button className="Button" onClick={ListDes}> Almuerzo y Cena</button>
+          <button className="Button" onClick={ListDes}>Desayuno</button>
+          <button className="Button" onClick={ListAlm}> Almuerzo y Cena</button>
           </div>
       </main>
   </div>
