0aba6da6b814555e2d0c685e5bbd4150562d6b5e
MARYRIVERO <mariyndlvalle892286@gmail.com>
2020-03-17 17:28:01 -0500

    corrige código

--- burguer-queen/src/Components/component-itemproducto.jsx
+++ burguer-queen/src/Components/component-itemproducto.jsx
@@ -3,22 +3,17 @@ import React from "react";
 const ItemProducto = ({ dataProducto }) => {
   console.log('vista primo', dataProducto)
   return (
-    <section className="itemproducto">
-      <p>{dataProducto.producto}</p>
-      <p>{dataProducto.precio}</p>
-      <input
-        type="number"
-        min="1"
-        max="100"
-        defaultValue={dataProducto.cantidad}
-        onClick={(e) => {
-          const p = e.target.value;
-          dataProducto.cantidad = (dataProducto.id, p);
-        }}
-      />
-      S/.
-      {dataProducto.precio * dataProducto.cantidad}
-
+    <section className="itemproducto">      
+          <input type="number" min="1" max="100"
+          defaultValue={dataProducto.cantidad}
+          onClick={(e) => {
+            const p = e.target.value;
+            dataProducto.cantidad = (dataProducto.id, p);
+          }}
+        />
+          <p>{dataProducto.producto}</p>
+          <p>{dataProducto.precio}</p>
+          <p> S/. {dataProducto.precio * dataProducto.cantidad}</p>
     </section>
   )
 }
