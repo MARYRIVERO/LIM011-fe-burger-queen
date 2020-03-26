6b65798612b0409be6c49eaced82ca76bde09425
MARYRIVERO <mariyndlvalle892286@gmail.com>
2020-03-26 18:35:28 -0500

    actualiza test

---
+++ burguer-queen/src/test/Menu.spec.js
@@ -0,0 +1,30 @@
+// import React from 'react';
+// import { render } from '@testing-library/react'; 
+// import  Menu from '../Components/component-menu';
+
+
+
+// const { getByText } = render(<Menu />)
+// const aboutAnchorNode = getByText(/about/i)
+
+// describe('Header', () => {
+//     it('Deberia mostrar el boton desayunno de la Aplicacion', () => {
+
+
+
+//     const { getByText } = render(<Menu />)
+//     const element = getByText('Desayuno');
+//     expect(element). 
+//     })
+//   })
+  
+
+
+
+//   describe('Header', () => {
+//     it('Deberia mostrar el logo de la Aplicacion', () => {
+//       const { getByTestId } = render(<Logo/>)
+//       const element = getByTestId('imagen');
+//       expect(element).toBeTruthy()
+//     })
+//   })
