91162337ecdcdeb3c65ac639e0f9e3e8f93b0455
MariangelMM <mariangelmoramarquez@gmail.com>
2020-03-27 18:53:09 -0500

    agrega test de componente menu

--- burguer-queen/src/test/Header.spec.js
+++ burguer-queen/src/test/Header.spec.js
@@ -1,4 +1,3 @@
-/* eslint-disable no-unused-vars */
 import React from 'react';
 import { render } from '@testing-library/react'; 
 import Logo from '../Components/component-header';
@@ -11,28 +10,3 @@ describe('Header', () => {
   })
 })
 
-
-// test('beber La Croix lleva a conseguir info de sed', () => {
-//   beberPocoLaCroix();
-//   expect(infoSed()).toBeTruthy();
-// });
-
-// describe('Registro de nuevo usuario', () => {
-//   it('debería registrar un usuario nuevo', () => {
-//     createUser('prueba-de-red@gmail.com, abc123').then((user) => {
-//       expect(user.email).toBe('prueba-de-red');
-//     });
-//   });
-// });
-
-// describe('ordenadorAZ', () => {
-//   it('debería ser una función', () => {
-//     expect(typeof ordenadorAZ).toBe('function');
-//   });
-//   it('debería ordenar alfabéticamente de A-Z', () => {
-//     expect(ordenadorAZ(potterPrueba, 'ordenar-az')[0].name).toEqual('Argus Filch');
-//   });
-//   it('debería ordenar alfabéticamente de Z-A', () => {
-//     expect(ordenadorAZ(potterPrueba, 'ordenar-za')[0].name.reverse).toEqual();
-//   });
-// });
---
+++ burguer-queen/src/test/Menu.spec.js
@@ -0,0 +1,72 @@
+import React from 'react';
+import { render, fireEvent, wait } from '@testing-library/react';
+import Menu from '../Components/component-menu';
+
+
+
+it('Deberia cambiar los productos al dar click', (done) => {
+
+    const fnagregar = jest.fn();
+
+    const containerMenu  = render( <Menu agregar={ fnagregar }/> );
+
+    
+    wait(() => containerMenu.getByText('Hamburguesas') ).then(() => {
+    
+    const antes = containerMenu.getAllByAltText('producto').length;
+    
+       
+    const button = containerMenu.getByText('Hamburguesas');
+
+    // disparando el evento de click del button
+   
+    fireEvent.click(button);
+
+    wait(() => containerMenu.getAllByAltText('producto')).then(() => {
+
+    const despues = containerMenu.getAllByAltText('producto').length
+    console.log(antes);
+    console.log(despues);
+        expect(antes).not.toEqual(despues);
+     done();
+    })
+   })
+    });
+
+
+
+
+
+// it('Deberia limpiar el input al dar click al button', () => {
+//   // creando prop 
+
+//   // renderizando el componente
+//   const { getByText, getByPlaceholderText } = render(
+//     <FormAgregarTarea agregarTarea={ (data) => { console.log(data) } }/>
+//   );
+
+//   // obteniendo el input por su placeholder
+//   const input = getByPlaceholderText('agrega una tarea');
+
+//   // obteniendo el button por su texto
+//   const button = getByText('Agregar');
+  
+//   // valor del input al renderizar el componente
+//   expect(input.value).toBe('');
+
+//   // asignando valor al input
+//   act(() => {
+//     fireEvent.change(input, { target: { value: 'hacer los tests' } })
+//   })
+
+//   // verificando que el valor del input fur modificado
+//   expect(input.value).toBe('hacer los tests');
+
+//   // disparando el evento de click del button
+//   act(() => {
+//     fireEvent.click(button);
+//   })
+  
+//   // despues de dar click al button
+//   expect(input.value).toBe('');
+// });
