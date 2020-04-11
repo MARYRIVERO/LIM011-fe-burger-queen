80b53d3058985b5816399be3128d3d7713de3478
MARYRIVERO <mariyndlvalle892286@gmail.com>
2020-04-10 21:49:39 -0500

    Despliega proyecto

--- src/Routes.js
+++ src/Routes.js
@@ -1,15 +1,18 @@
 
 import React from 'react';
-import {Switch, Route} from 'react-router-dom';
+import {HashRouter,Switch, Route} from 'react-router-dom';
 import ComponenteVistaMesero from './Components/componente-vista-mesero';
 import ComponenteVistaChef from './Components/componente-vista-chef';
 
 const Routes = () => {
       return(
-        <Switch>
-           <Route exact path='/' component={ComponenteVistaMesero} />
-           <Route path='/chef' component={ComponenteVistaChef} />
-        </Switch>
+        <HashRouter>
+          <Switch>
+            <Route exact path='/' component={ComponenteVistaMesero} />
+            <Route path='/chef' component={ComponenteVistaChef} />
+          </Switch>
+        </HashRouter>
+
 
       )}
 export default Routes;
