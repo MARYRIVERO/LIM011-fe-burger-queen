60a77460d5b5e08ac2c247e90abbbcb3649558b0
MARYRIVERO <mariyndlvalle892286@gmail.com>
2020-04-10 21:59:49 -0500

    Updates

---
+++ precache-manifest.fc9ba51ce85ddca8d57dd97171f9afd4.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8da149e73d340eb13978ff5b48bbf184",
+    "url": "/LIM011-fe-burger-queen/index.html"
+  },
+  {
+    "revision": "69b05facb8b4f73473ad",
+    "url": "/LIM011-fe-burger-queen/static/css/main.6e705bd5.chunk.css"
+  },
+  {
+    "revision": "c8b1265f76bbd233f47b",
+    "url": "/LIM011-fe-burger-queen/static/js/2.34f83863.chunk.js"
+  },
+  {
+    "revision": "7ba5a91c6aa4ae9cc068a25fb13d169c",
+    "url": "/LIM011-fe-burger-queen/static/js/2.34f83863.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "69b05facb8b4f73473ad",
+    "url": "/LIM011-fe-burger-queen/static/js/main.befd16a2.chunk.js"
+  },
+  {
+    "revision": "df30202cd30acd3c1699",
+    "url": "/LIM011-fe-burger-queen/static/js/runtime-main.79204ea3.js"
+  },
+  {
+    "revision": "9829bde7dca904dc4b7f1e7684dabf4e",
+    "url": "/LIM011-fe-burger-queen/static/media/fondoBurger.9829bde7.svg"
+  },
+  {
+    "revision": "815dae7ee10ffd1d6f9039c2c4309ca3",
+    "url": "/LIM011-fe-burger-queen/static/media/fondoHeader.815dae7e.svg"
+  },
+  {
+    "revision": "41f17378fa14d06793f8263773e3e426",
+    "url": "/LIM011-fe-burger-queen/static/media/logoReina.41f17378.svg"
+  }
+]);
